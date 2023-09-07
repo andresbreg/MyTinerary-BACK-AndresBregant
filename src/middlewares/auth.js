1273d608926ae2d5121bc155a2561a3aa5d874d8
andresbregant <andresbregant@gmail.com>
2023-09-07 12:36:18 -0300

    Bcrypt

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,48 +1,15 @@
-const Joi = require('joi')
+const bcrypt = require('bcrypt')
 
-const userSchema = Joi.object({
-  name: Joi.string().min(2).max(24).required().messages({
-    'string.empty': 'Please, enter your name.',
-    'string.min': 'The name must be at least 2 characters.',
-    'string.max': 'The name must be at most 24 characters.'
-  }),
-  last_name: Joi.string().min(2).max(24).required().messages({
-    'string.empty': 'Please, enter your last name.',
-    'string.min': 'The last name must be at least 2 characters.',
-    'string.max': 'The last name must be at most 24 characters.'
-  }),
-  photo: Joi.string().uri().min(8).messages({
-    'string.uri': 'Please, enter a valid location.',
-    'string.min': 'The photo location must be at least 8 characters.'
-  }),
-  country: Joi.string().required().messages({
-    'string.empty': 'Please, select your country.',
-  }),
-  email: Joi.string().email().min(8).max(48).required().messages({
-    'string.empty': 'Please, enter your email.',
-    'string.email': 'Please, enter a valid email.',
-    'string.min': 'The email must be at least 8 characters.',
-    'string.max': 'The email must be at most 48 characters.'
-  }),
-  password: Joi.string().alphanum().min(8).max(24).required().messages({
-    'string.empty': 'Please, enter a password.',
-    'string.alphanum': 'Please, enter a valid password.',
-    'string.min': 'The password must be at least 8 characters.',
-    'string.max': 'The password must be at most 24 characters.'
-  })
-})
-
-const verifyAuthData = (req,res,next) => {
-  const payload = req.body
-  const validatedUser = userSchema.validate(payload)
-  if (validatedUser.error) {
-    return res.status(400).json({
-      messages: validatedUser.error.details.map(
-        error => error.message
-      )
-    })
+const hashPassword = (req,res,next) => {
+  try {
+    const password = req.body.password
+    const hashPassword = bcrypt.hashSync(password, 10)
+    req.body.password = hashPassword
+    next()
   }
-  next()
+  catch (error) {
+    res.status(500).json({message: error.message})
+  }
 }
 
-module.exports = {verifyAuthData}
+module.exports = {hashPassword}
--- src/router/auth.js
+++ src/router/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const {verifyAuthData} = require('../middlewares/auth')
+const {verifyAuthData} = require('../middlewares/user')
+const {hashPassword} = require('../middlewares/auth')
 const {registerUser} = require('../controllers/authController')
 
 const authRouter = express.Router()
 
-authRouter.post('/register', verifyAuthData, registerUser)
+authRouter.post('/register', verifyAuthData, hashPassword, registerUser)
 
 module.exports = authRouter
